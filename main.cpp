ce873fb872fcb24cd26ed667379471add9338ee8
Filip Žitný <filipzitny@gmail.com>
2023-10-04 08:57:13 +0200

    Size of array

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+void sizeOfArray(int n) {
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+//        arr[i] = ;  add random variable
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    delete [] arr;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int N;
+
+    std::cout << "Enter a size of the array: ";
+    std::cin >> N;
+
+    sizeOfArray(N);
+
     return 0;
 }
